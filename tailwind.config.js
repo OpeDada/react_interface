ae2084d2ac32eea7453f2e7dcfd72c2cb0455451
OpeDada <opedada@gmail.com>
2023-06-22 10:29:39 +0900

    debug tailwind css and craco

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
+/* @type {import('tailwindcss').Config} */
+
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -7,5 +9,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms')],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
